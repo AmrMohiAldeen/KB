24506fc373265c603279a824d00c430241bcefff
unknown <amrmohi117@gmail.com>
2026-08-16 06:19:15 +0400

    imgration fixes 1.19

--- backend/kb-backend/Database/Migrations/search.sql
+++ backend/kb-backend/Database/Migrations/search.sql
@@ -1,92 +1,85 @@
-use [KB];
-SET XACT_ABORT ON;
+USE KB;
+GO
 
-BEGIN TRY
-    BEGIN TRANSACTION;
+------------------------------------------------------------
+-- CATEGORIES.Visibility
+------------------------------------------------------------
 
-    ------------------------------------------------------------
-    -- IndexScope
-    ------------------------------------------------------------
-    IF COL_LENGTH('dbo.SEARCH_INDEX_JOBS', 'IndexScope') IS NULL
-    BEGIN
-        ALTER TABLE dbo.SEARCH_INDEX_JOBS
-        ADD IndexScope nvarchar(30) NULL;
-    END;
+IF COL_LENGTH('dbo.CATEGORIES', 'Visibility') IS NULL
+BEGIN
+    ALTER TABLE dbo.CATEGORIES
+    ADD Visibility nvarchar(20) NOT NULL
+        CONSTRAINT DF_CATEGORIES_Visibility DEFAULT N'Public';
+END;
+GO
 
-    UPDATE dbo.SEARCH_INDEX_JOBS
-    SET IndexScope = N'Internal'
-    WHERE IndexScope IS NULL
-       OR LTRIM(RTRIM(IndexScope)) = N'';
+IF OBJECT_ID(N'dbo.CK_CATEGORIES_Visibility', N'C') IS NULL
+BEGIN
+    ALTER TABLE dbo.CATEGORIES
+    ADD CONSTRAINT CK_CATEGORIES_Visibility
+    CHECK (Visibility IN (N'Public', N'Internal'));
+END;
+GO
 
+
+------------------------------------------------------------
+-- ARTICLES.Visibility
+------------------------------------------------------------
+
+IF COL_LENGTH('dbo.ARTICLES', 'Visibility') IS NULL
+BEGIN
+    ALTER TABLE dbo.ARTICLES
+    ADD Visibility nvarchar(20) NOT NULL
+        CONSTRAINT DF_ARTICLES_Visibility DEFAULT N'Public';
+END;
+GO
+
+IF OBJECT_ID(N'dbo.CK_ARTICLES_Visibility', N'C') IS NULL
+BEGIN
+    ALTER TABLE dbo.ARTICLES
+    ADD CONSTRAINT CK_ARTICLES_Visibility
+    CHECK (Visibility IN (N'Public', N'Internal'));
+END;
+GO
+
+
+------------------------------------------------------------
+-- SEARCH_INDEX_JOBS.IndexScope
+------------------------------------------------------------
+
+IF COL_LENGTH('dbo.SEARCH_INDEX_JOBS', 'IndexScope') IS NULL
+BEGIN
     ALTER TABLE dbo.SEARCH_INDEX_JOBS
-    ALTER COLUMN IndexScope nvarchar(30) NOT NULL;
+    ADD IndexScope nvarchar(30) NULL;
+END;
+GO
 
-    IF OBJECT_ID(
-        N'dbo.DF_SEARCH_INDEX_JOBS_IndexScope',
-        N'D'
-    ) IS NULL
-    BEGIN
-        ALTER TABLE dbo.SEARCH_INDEX_JOBS
-        ADD CONSTRAINT DF_SEARCH_INDEX_JOBS_IndexScope
-        DEFAULT N'Internal' FOR IndexScope;
-    END;
+UPDATE dbo.SEARCH_INDEX_JOBS
+SET IndexScope = N'Internal'
+WHERE IndexScope IS NULL;
+GO
+
+ALTER TABLE dbo.SEARCH_INDEX_JOBS
+ALTER COLUMN IndexScope nvarchar(30) NOT NULL;
+GO
 
 
-    ------------------------------------------------------------
-    -- AvailableAt
-    ------------------------------------------------------------
-    IF COL_LENGTH('dbo.SEARCH_INDEX_JOBS', 'AvailableAt') IS NULL
-    BEGIN
-        ALTER TABLE dbo.SEARCH_INDEX_JOBS
-        ADD AvailableAt datetime2(3) NULL;
-    END;
+------------------------------------------------------------
+-- SEARCH_INDEX_JOBS.AvailableAt
+------------------------------------------------------------
 
-    UPDATE dbo.SEARCH_INDEX_JOBS
-    SET AvailableAt = SYSUTCDATETIME()
-    WHERE AvailableAt IS NULL;
-
+IF COL_LENGTH('dbo.SEARCH_INDEX_JOBS', 'AvailableAt') IS NULL
+BEGIN
     ALTER TABLE dbo.SEARCH_INDEX_JOBS
-    ALTER COLUMN AvailableAt datetime2(3) NOT NULL;
+    ADD AvailableAt datetime2(3) NULL;
+END;
+GO
 
-    IF OBJECT_ID(
-        N'dbo.DF_SEARCH_INDEX_JOBS_AvailableAt',
-        N'D'
-    ) IS NULL
-    BEGIN
-        ALTER TABLE dbo.SEARCH_INDEX_JOBS
-        ADD CONSTRAINT DF_SEARCH_INDEX_JOBS_AvailableAt
-        DEFAULT SYSUTCDATETIME() FOR AvailableAt;
-    END;
+UPDATE dbo.SEARCH_INDEX_JOBS
+SET AvailableAt = SYSUTCDATETIME()
+WHERE AvailableAt IS NULL;
+GO
 
-
-    ------------------------------------------------------------
-    -- Useful worker polling index
-    ------------------------------------------------------------
-    IF NOT EXISTS (
-        SELECT 1
-        FROM sys.indexes
-        WHERE object_id = OBJECT_ID(N'dbo.SEARCH_INDEX_JOBS')
-          AND name = N'IX_SEARCH_INDEX_JOBS_Poll'
-    )
-    BEGIN
-        CREATE INDEX IX_SEARCH_INDEX_JOBS_Poll
-        ON dbo.SEARCH_INDEX_JOBS
-        (
-            IndexScope,
-            Status,
-            AvailableAt
-        )
-        INCLUDE
-        (
-            ProcessedAt
-        );
-    END;
-
-    COMMIT TRANSACTION;
-END TRY
-BEGIN CATCH
-    IF @@TRANCOUNT > 0
-        ROLLBACK TRANSACTION;
-
-    THROW;
-END CATCH;
+ALTER TABLE dbo.SEARCH_INDEX_JOBS
+ALTER COLUMN AvailableAt datetime2(3) NOT NULL;
+GO
